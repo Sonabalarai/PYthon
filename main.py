fbf8d1f5bd29e901657453097631ce38af02fc2d
Sona Rai <sonabalarai@gmail.com>
2026-09-06 20:35:30 +0545

    file handling

---
+++ main.py
@@ -0,0 +1,196 @@
+import random
+# print("helo guys")
+
+# a ="Hello how are you"
+# print(a[5:9])
+# print(a[9:13])
+# print(a[13:17])
+
+# n1 = int(input("enter the 1st number: "))
+# n2 = int(input("enter the 1st number: "))
+
+# if n1>n2:
+#     print(n1)
+# else : 
+#     print(n2)
+
+# gender = input("enter the your gender: ")
+
+# if gender == "M" or gender=="m" or gender=="Male" or gender=="male":
+#     print("Hello Good morning sir")
+# elif gender=='F' or gender=='f' or gender=='Female' or gender=='female':
+#     print("goo morning mam")
+# else :
+#     print("good morning sir or madam")
+
+# num = int(input("enter the number: "))
+
+# if num%2==0:
+#     print("even")
+# else :
+#     print("odd")
+
+# year = int(input("enter your year: "))
+
+# if year%100==0 and year % 400 ==0: #centure n leap year
+#     print(f" {year} is a leap year")
+# elif year%100!=0 and year % 4==0 : #normal year
+#     print(f"{year} is  a leap year")
+# else:
+#     print("not a leap year")
+
+#multiplication of 5
+ 
+# for i in range(1,11):
+#     print(f"5 * {i}","=",i*5)
+
+# num = int(input("enter your number:-"))
+# for i in range(num,0,-1):
+#     print(i)
+
+# n= int(input("enter your number;-"))
+
+# sum=0
+# for i in range(1,n+1):
+#     sum+=i
+# print(sum)
+
+# n = int(input("enter your number:-"))
+# even=0
+# odd=0
+# for i in range(1,n+1):
+#     if i%2==0:
+#         even+=i
+#     else :
+#         odd+=i        
+# print(even, odd)
+
+# n = int(input("enter your number:-"))
+# count=0
+# p_sum=0
+# for i in range(1,n+1):
+#     if n%i==0:
+#         p_sum+=i
+#         count+=1
+# if count==2:
+#     print(f"{n} is a prime number")
+# else :
+#     print(f"{n} is not a prime number")
+
+# str = "Python"
+# rev = ""
+# # print(str[-1: : -1])
+# for i in range(len(str)-1,-1,-1):
+#     rev = rev + str[i]
+# print(rev)
+
+# a = "ABc"
+# rev1 =""
+# # rev2 =""
+
+# for i in range(len(a)-1,-1,-1):
+#     rev1=rev1 + a[i]
+# # for i in range(len(a)-1,-1,-1):
+# #     rev2 = rev2+ a[i]
+# if rev1==a:
+#     print(f"{a} is a pallindrome string")
+# else :
+#     print(f"{a} is not a pallindrome string")
+    
+# print(rev1,rev2)
+
+
+val ="12837jsnfslido%^un&&"
+digit= 0
+char = 0
+spchar =0
+
+# for i in val:
+#     if i.isdigit():
+#         dihgit+=1
+#     elif i.isalpha():
+#         char+=1
+#     else :
+#         spchar+=1
+# print(char)
+# print(spchar)
+# print(dihgit)
+
+# print(ord("A"),ord("Z"))
+# print(ord("a"),ord("z"))
+# print(ord("0"),ord("9"))
+
+# for i in val:
+#     if (ord(i) >=65 and ord(i)<=90) or (ord(i)>=97 and ord(i)<=122):
+#         char+=1
+#     elif ord(i)>=48 and ord(i)<=57 :
+#         digit+=1
+#     else:
+#         spchar+=1
+# print(f"characters - {char} , digits - {digit}, special character - {spchar}")        
+
+# a=1243
+# a = str(a)
+# for i in range(0,len(a),1):
+    
+#     print(a[i])
+
+# QQQ
+
+# a = int(input("enter your number:"))
+# r=0
+# while a != 0:
+#     rem=a%10
+#     print(rem)
+#     a=a//10
+ 
+# PRINT IN REVERSE ORDER   
+# a = int(input("enter your number:"))
+# r=0
+# while a != 0:
+#     r=r*10+a%10
+#     print(r)
+#     a=a//10    
+
+# com = random.randint(1,100)
+
+# tries = 0
+
+# while True:
+#     tries +=1
+#     hum= int(input("enter your guess:-"))
+    
+#     if hum==com :
+#         print("Conguralation you have won the game!")
+#         break
+#     elif hum>com:
+#         print("you have guess higher value , please go for lower value !")
+#     elif hum<com:
+#         print("you have guess lower value so please go for litter higher value !")
+#     else:
+#         print("your out of range")
+# print(f"Completed in {tries} tries")
+
+choices =["rock","scissor","paper"]
+com = random.choice(choices)
+
+
+while True:
+    player = input("enter your guess among [rock or scissor or paper] :- ")
+    
+    if player =="quit":
+        print("thank for your time")
+        break
+    
+    if com == player:
+        print("Draw game, please guess next")
+    elif (com == "rock" and player =="scissor") or (com=="paper" and player=="rock") or(com=="scissor" and player=="paper"):
+        print("you lose the game. You can play again")
+        
+    elif (com =="paper" and player=="scissor") or (com=="scissor" and player=="rock") or (com=="rock" and player=="paper"):
+        print("you won the game .Conguratulation !")
+        break
+    else:
+        print("Invalid ! guess the possible choice !")
+        
+    
---
+++ part2.py
@@ -0,0 +1,188 @@
+# Function 
+# def add(a,b,c):
+#     print(a+b)
+    
+# add(29,10)
+
+# Argument type are of three type
+
+# 1)Positional argument
+# above example is positional argument  thAT is well we pass the argument
+
+# lis= ["fruit","appla"]
+# print(lis)
+# l=list(lis[1])
+# l[4]="e"
+# lis1="".join(l)
+# lis[1]=lis1
+# print(lis)
+# print(l)
+
+num=[1,2,5,72,3,5,2,2]
+
+# seen = set()
+# final_list=[]
+# for i in num:
+#     if i in seen:
+#         if i not in final_list:
+#             final_list.append(i)
+#     else:
+#         seen.add(i)
+        
+# print(final_list) 
+
+
+# Q find the largest value from list and return its index as well
+# n = [12,89,-10,26,40,100]
+# large = n[0]
+# a=0
+# for i in n:
+#     if i>large:
+#         large = i
+#         a=n.index(i)
+        
+        
+# print(large,a)
+
+#Q find the second largest value in the list
+
+# a =[12,89,90,1000,23,999]
+
+# large = a[0]
+# sec_large = a[0]
+
+# for i in a:
+#     if i > large:
+#         sec_large=large
+#         large = i
+#     elif i>sec_large:
+#         sec_large=i
+        
+# print(f"The largest number is {large} and second largest number is {sec_large}" )
+
+
+# Q is the list is already sorted check
+
+# a= [2,57,1,0,8]
+# a=[1,2,3,4]
+
+# for i in range(len(a)-1):
+#     if a[i]>a[i+1]:
+#         print("Your list is not sorted")
+#         break
+# else:
+#     print("Your list is sorted")
+
+
+
+# QTuple 
+# def pack_unpack():
+#     return "sona",27,"sona@gmail.com"  
+
+
+# info = pack_unpack()
+# # unpack 
+# name, age, email = info
+# print(info)
+# print(name)
+# print(age)
+# print(email)
+
+
+tup = (1,2,"sona",80,3,3,1,"sona")
+
+# print(tup.index(2))
+
+# print(tup.count(3))
+# a,b,c,d,e=0,0,0,0,0
+# rep=[]
+# for i in range(0,len(tup)-1):
+    
+#     if tup.count(tup[i])>1:
+#         rep.append(tup[i])
+    
+
+# s=set(rep)
+
+# print(list(s))
+
+# print(type(l)) 
+
+# Q merge two dictionaries
+
+# d1 ={"a":10,"b":20,"c":30}
+# d2 = {"d":10,"e":20,"f":30}
+
+# # d1.update(d2)
+# # print(d1)
+
+# for i in d2:
+#     d1[i] =d2[i]
+# print(d1)
+
+
+#Q   SUM
+
+# d1 ={"a":10,"b":20,"c":30}
+
+# sum = 0
+# for i in d1:
+#     sum+=d1[i]
+    
+# print(sum)
+
+# Q2 count the frequency of each element in a list using a dicionary
+
+# l = ["a","b","a","c","b","a"]
+# d={}
+
+# for i in l:
+#     if i in d:
+#         d[i]+=1
+        
+#     else :
+#         d[i]=1
+# print(d)
+
+
+# Q
+# d1 ={"a":10,"b":20,"c":30}
+# d2 = {"c":30,"d":40,"e":50}
+
+# for i in d2:
+#     if i in d1:
+#         d1[i] +=d2[i]
+#     else:
+#         d1[i]=d2[i]
+        
+# print(d1)
+
+# Q)Try , exception handling, else, finally
+
+# n1=20
+# n2 = 2
+
+# try:
+#     print(n1/n2)
+# except Exception as err:
+#     print(f"Sorry we got an error {err}")
+    
+# else:
+#     print("no error")
+    
+# finally:
+#     print("If there is error or no errr i will run always")
+    
+
+# Try , exception , raise=>make or throw your wn custom error
+
+age=int(input("enter your age:-"))
+
+if age<18:
+    raise TypeError("you are not eligible for voting")
+
+print("you RE Eligible")
+
+
+    
+
